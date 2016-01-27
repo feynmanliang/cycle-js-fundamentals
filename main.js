4fc2e200e111a1e6fbe60d8ca858175ac84fb0cb
Feynman Liang <feynman.liang@gmail.com>
2016-01-27 21:33:52 +0000

    Adds sources

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 import Rx from 'rxjs';
 
 // Logic (functional)
-function main() {
+function main(DOMSource) {
+  const click$ = DOMSource;
   return {
-    DOM: Rx.Observable.timer(0, 1000)
-      .map(i => `Seconds elapsed ${i}`),
+    DOM: click$
+      .startWith(null)
+      .switchMap(() =>
+        Rx.Observable.timer(0, 1000)
+        .map(i => `Seconds elapsed ${i}`),
+      ),
     Log: Rx.Observable.timer(0, 2000).map(i => 2*i),
   };
 }
+
+// source: input (read) effects
+// since: output (write) effects
 
 // Drivers (aka effects, imperative)
 // OS drivers = interface between software and hardware
@@ -17,6 +25,9 @@ function DOMDriver(text$) {
     const container = document.querySelector('#app');
     container.textContent = text;
   });
+  // DOMSource ...
+  const DOMSource = Rx.Observable.fromEvent(document, 'click');
+  return DOMSource;
 }
 
 function consoleLogDriver(msg$) {
@@ -24,11 +35,14 @@ function consoleLogDriver(msg$) {
 }
 
 // Wiring (different) sinks to (different) effects
-function run(mainFn, effects) {
-  const sinks = mainFn();
-  Object.keys(effects).forEach(key => {
-    effects[key](sinks[key]);
-  })
+function run(mainFn, drivers) {
+  const proxyDOMSource = new Rx.Subject();
+  const sinks = mainFn(proxyDOMSource);
+  const DOMSource = drivers.DOM(sinks.DOM);
+  DOMSource.subscribe(click => proxyDOMSource.next(click));
+  //Object.keys(drivers).forEach(key => {
+  //  drivers[key](sinks[key]);
+  //})
 }
 
 // Effects keys must match sink keys
