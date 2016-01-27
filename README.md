6c210419aa75492e75a916ec9799da86fdcd2a75
Feynman Liang <feynman.liang@gmail.com>
2016-01-27 21:04:59 +0000

    Adds readme

---
+++ README.md
@@ -0,0 +1,4 @@
+Cycle.js Fundamentals
+=====================
+
+Implementation of example code from [egghead.io Cycle.js tutorial](https://egghead.io/lessons/rxjs-the-cycle-js-principle-separating-logic-from-effects).
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import Rx from 'rxjs';
 
-// Logic
+// Logic (functional)
 Rx.Observable.timer(0, 1000)
   .map(i => `Seconds elapsed ${i}`)
-// Effects
+// Effects (imperative)
   .subscribe(text => {
     const container = document.querySelector('#app');
     container.textContent = text;
