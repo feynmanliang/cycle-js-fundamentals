9ad3a8605e0836b60ea7d45e0f0d73f621a6f9bb
Feynman Liang <feynman.liang@gmail.com>
2016-01-27 21:03:43 +0000

    Simple timer example with logic/effects seperation

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 import Rx from 'rxjs';
 
+// Logic
+Rx.Observable.timer(0, 1000)
+  .map(i => `Seconds elapsed ${i}`)
+// Effects
+  .subscribe(text => {
+    const container = document.querySelector('#app');
+    container.textContent = text;
+  })
