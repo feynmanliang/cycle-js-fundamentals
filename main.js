69c78520c6e2245124f0602977d0077ae2ec9340
Feynman Liang <feynman.liang@gmail.com>
2016-01-28 13:11:57 +0000

    Complete cycle.js hello world

--- main.js
+++ main.js
@@ -2,54 +2,25 @@ import Rx from 'rx';
 import Cycle from '@cycle/core';
 import CycleDOM from '@cycle/dom';
 
-const { h, h1, span, makeDOMDriver } = CycleDOM;
+const { label, input, h1, hr, div, makeDOMDriver } = CycleDOM;
 
-// Logic (functional)
 function main(sources) {
-  const mouseover$ = sources.DOM.select('span').events('mouseover');
-  const sinks = {
-    DOM: mouseover$
-      .startWith(null)
-      .flatMapLatest(() =>
-          Rx.Observable.timer(0, 1000)
-            .map(i =>
-               h1([
-                 span([
-                   `Seconds elapsed: ${i}`
-                 ])
-               ]))),
-    Log: Rx.Observable.timer(0, 2000).map(i => 2*i),
+  const inputEv$ = sources.DOM.select('.field').events('input');
+  const name$ = inputEv$.map(ev => ev.target.value).startWith('');
+  return {
+    DOM: name$.map(name =>
+      div([
+        label('Name: '),
+        input('.field', {type: 'text'}),
+        hr(),
+        h1(`Hello ${name}!`)
+      ])
+    )
   };
-  return sinks;
 }
 
-// source: input (read) effects
-// since: output (write) effects
-
-// Drivers (aka effects, imperative)
-// OS drivers = interface between software and hardware
-// Here, hardware = effects, drivers = interfaces between logic and effects
-function consoleLogDriver(msg$) {
-  msg$.subscribe(msg => console.log(msg));
-}
-
-// Wiring (different) sinks to (different) effects
-function run(mainFn, drivers) {
-  const proxySources = {};
-  Object.keys(drivers).forEach(key => {
-    proxySources[key] = new Rx.Subject();
-  });
-  const sinks = mainFn(proxySources);
-  Object.keys(drivers).forEach(key => {
-    const source = drivers[key](sinks[key]);
-    source.subscribe(x => proxySources[key].onNext(x))
-  })
-}
-
-// Effects keys must match sink keys
 const drivers = {
   DOM: makeDOMDriver('#app'),
-  Log: consoleLogDriver,
 }
 
 Cycle.run(main, drivers);
