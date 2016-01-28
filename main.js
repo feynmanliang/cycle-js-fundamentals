27cc59811c588998a25f69627db8de019c61bfbc
Feynman Liang <feynman.liang@gmail.com>
2016-01-28 23:40:07 +0000

    Clean counter, include http driver

--- main.js
+++ main.js
@@ -1,35 +1,17 @@
 import Rx from 'rx';
 import Cycle from '@cycle/core';
 import CycleDOM from '@cycle/dom';
+import CycleHTTPDriver from '@cycle/http';
 
 const {button, p, label, div, makeDOMDriver } = CycleDOM;
+const { makeHTTPDriver } = CycleHTTPDriver
 
 function main(sources) {
-  const incrementClick$ = sources.DOM
-    .select('.increment').events('click');
-  const decrementClick$ = sources.DOM
-    .select('.decrement').events('click');
-  const incrementAction$ = incrementClick$.map(ev => +1);
-  const decrementAction$ = decrementClick$.map(ev => -1);
-
-  const number$ = Rx.Observable.of(0)
-    .merge(decrementAction$).merge(incrementAction$)
-    .scan((prev, curr) => prev + curr);
-  return {
-    DOM: number$.map(number =>
-      div([
-        button('.decrement', 'Decrement'),
-        button('.increment', 'Increment'),
-        p([
-          label(String(number))
-        ])
-      ])
-    )
-  };
 }
 
 const drivers = {
   DOM: makeDOMDriver('#app'),
+  HTTP: makeHTTPDriver()
 }
 
 Cycle.run(main, drivers);
