b8832f413301c021b5371fc731c7798f0c53753d
Feynman Liang <feynman.liang@gmail.com>
2016-01-29 00:13:05 +0000

    HTTP driver

--- main.js
+++ main.js
@@ -3,10 +3,46 @@ import Cycle from '@cycle/core';
 import CycleDOM from '@cycle/dom';
 import CycleHTTPDriver from '@cycle/http';
 
-const {button, p, label, div, makeDOMDriver } = CycleDOM;
+const { button, h1, h4, a, div, makeDOMDriver } = CycleDOM;
 const { makeHTTPDriver } = CycleHTTPDriver
 
+// button clicked (DOM read)
+// request sent (HTTP write)
+// response received (HTTP read)
+// data display (DOM write)
+
+
 function main(sources) {
+  const clickEv$ = sources.DOM
+    .select('.get-first').events('click');
+
+  const request$ = clickEv$.map(() => {
+    return {
+      url: 'http://jsonplaceholder.typicode.com/users/1',
+      method: 'GET',
+    };
+  });
+
+  const response$$ = sources.HTTP
+    .filter(response$ =>response$.request.url ===
+            'http://jsonplaceholder.typicode.com/users/1');
+  const response$ = response$$.switch();
+  const firstUser$ = response$.map(response => response.body)
+    .startWith(null);
+
+  return {
+    DOM: firstUser$.map(firstUser =>
+      div([
+        button('.get-first', 'Get first user'),
+        firstUser === null ? null : div('.user-details', [
+          h1('.user-name', firstUser.name),
+          h4('.user-email', firstUser.email),
+          a('.user-website', {href: firstUser.website}, firstUser.website)
+        ])
+      ])
+    ),
+    HTTP: request$,
+  }
 }
 
 const drivers = {
